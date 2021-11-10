fc2984c0dc9e5958ce101be4f8cce0c0d69a3109
Angelina Mikhailenko <92105372+AngelinaMB@users.noreply.github.com>
2021-11-11 02:08:45 +0300

    Add files via upload

--- lab_1/lab11.py
+++ lab_1/lab11.py
@@ -1,7 +1,5 @@
+import numpy
 import numpy as np
-import matplotlib.pyplot as plt
-data = [70, 10, 10, 10]
-with plt.xkcd():
-    plt.pie(data, labels=('В комментариях', 'В Ираке', 'В Сирии', 'В Афганистане'))
-    plt.title('Где ведутся самые ожесточенные бои')
-plt.show()
+x = [1, 10, 100]
+y = np.log(np.exp(1 / np.sin(x) + 1) / (5 / 4 + 1 / np.power(x, 15))) / np.log(1 + np.power(x, 2))
+print(y)
